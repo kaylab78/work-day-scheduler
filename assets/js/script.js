6a3acd11b267e85972e97a6ba2c524f82d6cfda5
kaylab78 <kaylabackus78@gmail.com>
2022-05-28 15:30:10 -0500

    Made description area editable with <textarea> element

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,30 +8,36 @@ var now = moment().format("LLLL");
 currentDayEl.textContent = now;
 
 // Print the hour to the timeBlockEl.
-timeBlockEl.textContent = moment().format("hh:mm a");
-
-// Check if time block is in the past, present or future using task auditing (5.5.4).
-function checkTime() {}
-
-// If time block is in the past, use class past.
-// If time block is in the present, use class present.
-// If time block is in the future, use class future.
+// timeBlockEl.textContent = moment().format("hh:mm a");
 
 // Click in the block to enter an event
-function createDescription() {}
+var meetings = [
+    {
+        hour: "8:00 pm",
+        description: ""
+    },
+    {
+        hour: "9:00 am",
+        description: ""
+    }
+];
 
-// Click out of the time block and the info stays
+function createDescription() {
+    // Prints corresponding hour to the page
+    timeBlockEl.append(meetings[0].hour)
 
-// Click the save button to save event in local storage
-function saveDescription() {
-    // localStorage.setItem("description", JSON.stringify(descriptionEl))
-    console.log("Clicked!");
-};
+    // Click in the description area to add or edit the text
+    // $(descriptionEl).click(function(){});
+    $(".description").on("click", "textarea", function() {
+        console.log("Clicked!");
+    })
 
-// Event delegation. I expect this to save the entire row to localStorage.
-$("div.row").on("click", "button", saveDescription);
+    loadDescription();
+}
 
-// Refresh the page and the saved events persist
+$(document).ready(createDescription());
+
+// Check if there are already events saved in localStorage. If none, set the descriptionEl to empty.
 function loadDescription() {
     descriptionEl = JSON.parse(localStorage.getItem("description"));
 
@@ -39,3 +45,22 @@ function loadDescription() {
         descriptionEl = {}
     };
 };
+
+// Click the save button to save event in local storage
+function saveDescription() {
+    localStorage.setItem("description", JSON.stringify(descriptionEl));
+};
+
+// Event delegation. I expect this to save the entire row to localStorage.
+$("div.row").on("click", "button", saveDescription);
+
+// Check if time block is in the past, present or future using task auditing (5.5.4).
+function checkTime() {
+    if (meetings[i].hour === moment().format("h:mm a")) {
+        $("div.row").addClass("present");
+    }
+}
+
+// If time block is in the past, use class past.
+// If time block is in the present, use class present.
+// If time block is in the future, use class future.
