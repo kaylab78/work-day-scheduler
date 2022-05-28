44d7e1c821d84cd5014e66746f549269a02f029b
kaylab78 <kaylabackus78@gmail.com>
2022-05-27 19:59:38 -0500

    Added styles to first row and columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var currentDayEl = document.getElementById("currentDay");
 var now = moment().format("LLLL");
 currentDayEl.textContent = now;
 
-// Check if time block is in the past, present or future.
+// Check if time block is in the past, present or future using task auditing (5.5.4).
 // If time block is in the past, use class past.
 // If time block is in the present, use class present.
 // If time block is in the future, use class future.
