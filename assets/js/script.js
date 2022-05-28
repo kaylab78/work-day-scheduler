94583e019b8826a498571da3c17af508f3f29e5b
kaylab78 <kaylabackus78@gmail.com>
2022-05-28 11:04:43 -0500

    Save button works when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,40 @@
 var currentDayEl = document.getElementById("currentDay");
+var timeBlockEl = document.querySelector(".time-block");
+var descriptionEl = document.querySelector(".description");
+var saveEl = document.querySelector(".saveBtn");
 
 // Print current date and time to header
 var now = moment().format("LLLL");
 currentDayEl.textContent = now;
 
+// Print the hour to the timeBlockEl.
+timeBlockEl.textContent = moment().format("hh:mm a");
+
 // Check if time block is in the past, present or future using task auditing (5.5.4).
+function checkTime() {}
+
 // If time block is in the past, use class past.
 // If time block is in the present, use class present.
 // If time block is in the future, use class future.
+
+// Click in the block to enter an event
+function createDescription() {}
+
+// Click out of the time block and the info stays
+
+// Click the save button to save event in local storage
+function saveDescription() {
+    // localStorage.setItem("description", JSON.stringify(descriptionEl))
+    console.log("Clicked!");
+};
+
+$(".saveBtn").click(saveDescription);
+
+// Refresh the page and the saved events persist
+function loadDescription() {
+    descriptionEl = JSON.parse(localStorage.getItem("description"));
+
+    if (!descriptionEl) {
+        descriptionEl = {}
+    };
+};
